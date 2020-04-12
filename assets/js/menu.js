b74c7462f8b48ef6fe3baf6b76b415a81eb28d9c
Andrew Swenson <swena56@gmail.com>
2020-04-12 17:08:19 -0500

    first commit

---
+++ assets/js/menu.js
@@ -0,0 +1,24 @@
+const resetRatio = () => {
+    // Find all iframes
+    var $iframes = $("iframe");
+
+    // Find &amp;amp;#x26; save the aspect ratio for all iframes
+    $iframes.each(function () {
+        $(this).data("ratio", this.height / this.width)
+            // Remove the hardcoded width &amp;amp;#x26; height attributes
+            .removeAttr("width")
+            .removeAttr("height");
+    });
+
+    // Resize the iframes when the window is resized
+    $(window).resize(function () {
+        $iframes.each(function () {
+            // Get the parent container&amp;amp;#x27;s width
+            var width = $(this).parent().width();
+            $(this).width(width)
+                .height(width * $(this).data("ratio"));
+        });
+        // Resize to fix all iframes on page load.
+    }).resize();
+};
+
